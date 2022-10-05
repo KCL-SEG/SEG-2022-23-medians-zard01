6a053748901fb8ff72e924fa17d2d8d45df7ba34
zard01 <89741647+zard01@users.noreply.github.com>
2022-10-05 02:10:59 +0100

    First attempt at solution
    Check if the size of the list of numbers is odd or even, then calculate median appropriately. For even, number that is half the size of the list will give index for right-middle number. For odd, the size of list -1 and halved will give index for middle number (-1 to account for 0-indexed)

--- median.py
+++ median.py
@@ -5,8 +5,14 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        if numbers.len()%2==0:
+            middle1=numbers[numbers.len()/2]
+            middle2=numbers[(numbers.len()/2)-1]
+            median = (middle1+middle2)/2
+        else:
+            median = numbers[(numbers.len()-1)/2]
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(median)
